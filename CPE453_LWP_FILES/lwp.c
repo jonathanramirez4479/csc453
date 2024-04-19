bd6507071e006683e5f3225bd6e87e4d8cb74b1c
Jonathan Ponce Ramirez <jrami245@unix1.csc.calpoly.edu>
2024-04-19 16:36:04 -0700

    working on new_lwp

---
+++ CPE453_LWP_FILES/lwp.c
@@ -0,0 +1,66 @@
+#include "stdio.h"
+#include "lwp.h"
+#include "stdlib.h"
+
+const unsigned int WORD_SIZE = 4;
+lwp_context lwp_ptable[LWP_PROC_LIMIT];
+int lwp_procs = 0;
+
+int new_lwp(lwpfun func, void *arg, size_t stack_size)
+{
+    /*
+    Notes:
+    - stacksize is the word size
+    - create a thread and store the data in the struct
+    - return the process id of the newly allocated thread
+        or -1 if more than LWP_PROC_LIMIT threads already exist
+    */
+
+   // check if maximum threads are being used
+   if (lwp_procs == LWP_PROC_LIMIT)
+   {
+    return -1;
+   }
+
+   int free_thread_index;
+   int i;
+    
+    // find an unused thread in the thread pool
+    for(i = 0; i <= LWP_PROC_LIMIT; i++)
+        if(lwp_ptable[i].stack == NULL)
+        {
+            free_thread_index = i;
+            break;
+        }
+
+    lwp_ptable[free_thread_index].pid = free_thread_index + 1;
+    lwp_ptable[free_thread_index].stacksize = stack_size;
+
+    lwp_ptable[free_thread_index].stack = (ptr_int_t*)malloc(stack_size * WORD_SIZE);
+
+    ptr_int_t* sp = lwp_ptable[free_thread_index].stack;
+    lwp_ptable[free_thread_index].sp = sp;
+    sp += (stack_size * WORD_SIZE);
+
+    *sp = (ptr_int_t)arg;
+    sp--;
+
+    *sp = (ptr_int_t) exit;
+    sp--;
+
+    *sp = (ptr_int_t) func;
+    sp--;
+
+    *sp = (ptr_int_t) 0xFEEDBEEF;
+
+    ptr_int_t* bp = (ptr_int_t) sp;
+    sp -= 7;
+
+    *sp = (ptr_int_t) bp;
+
+    SetSP(sp);
+    RESTORE_STATE();
+
+    return 5;
+
+}
---
+++ CPE453_LWP_FILES/proof_of_concept.c
@@ -0,0 +1,47 @@
+#include "stdio.h"
+#include "lwp.h"
+#include "stdlib.h"
+
+#define SetBP(bp) asm("movl  %0,%%ebp" : : "r"(bp))
+
+int test_fun(int x)
+{
+    x = x + 20;
+    printf("what did i get? %d\n", x);
+}
+
+int main()
+{
+    int arg[1];
+    arg[0] = 5;
+    // ptr_int_t *sp = (ptr_int_t *)malloc(8192);
+
+    // sp += 2048;
+    // sp--;
+
+    // *sp = (ptr_int_t) 100; // argument
+    // sp--;
+
+    // *sp = (ptr_int_t) exit;
+    // sp--;
+
+    // *sp = (ptr_int_t) test_fun;
+    // sp--;
+
+    // *sp = (ptr_int_t) 0xFEEDBEEF;
+
+    // ptr_int_t *bp = (ptr_int_t)sp;
+
+    // sp -= 7;
+    // *sp = (ptr_int_t)bp;
+
+    // SetSP(sp);
+    // RESTORE_STATE();
+
+    int res = new_lwp((lwpfun)test_fun, (void*) arg, 2048);
+
+    
+    // free(sp);
+
+    return 0;
+}
