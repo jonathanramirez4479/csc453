e97d1b9fa05a0b1248f9c1532f68f30a964fd9e2
jonathanramirez4479 <jonathanramirez4479@gmail.com>
2024-05-24 23:36:20 -0700

    done now

--- lab3/memSim.sh
+++ lab3/memSim.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Compile the Java program
-javac memSim.java
+# Compile the Java program in the src directory
+javac src/*.java
 if [ $? -ne 0 ]; then
     echo "Compilation failed"
     exit 1
 fi
 
 # Run the Java program with the provided arguments
-java memSim "$@"
+java src/memSim "$@"
