23d789d207ae064384a03678ec407903638eb934
jonathanramirez4479 <106727963+jonathanramirez4479@users.noreply.github.com>
2024-06-09 12:43:06 -0700

    fix readBytes to accept a char buffer, (string or char array), and test seek again, just to make sure it works correctly

--- lab4/main.py
+++ lab4/main.py
@@ -7,19 +7,14 @@ def main():
     tfs_mount(filename=DEFAULT_DISK_NAME)
 
     with open(DEFAULT_DISK_NAME, 'rb') as f:
-        # f.seek(0)
-        # print(f.read(BLOCK_SIZE)) # superblock
-        # print()
-        # f.seek(BLOCK_SIZE) # Root Dir
-        # print(f.read(BLOCK_SIZE))
 
         first_file_descriptor = tfs_open(name="foo.c")
-        print("fd for foo.c", first_file_descriptor)
 
-        write_status = tfs_write(first_file_descriptor, "hello world!", 12)
+        write_status = tfs_write(first_file_descriptor, "a"*257, 257)
         if write_status != DiskErrorCodes.SUCCESS:
             print(f"Write Error: {write_status}")
             return
+
         tfs_seek(2, 0)  # reset file pointer
         # Read each byte from the file
         read_bytes = []
@@ -30,22 +25,14 @@ def main():
                 return
             read_bytes.append(byte_value)
 
-        # Convert read bytes back to string
-        read_string = ''.join(chr(b) for b in read_bytes)
-        print(f"Read string: {read_string}")
+        print(f"Read Bytes: {read_bytes}")
 
-        # test_write_error = tfs_write(0, "hello world!", 9) #FD error
-        # print(test_write_error) # -8 error code
-        tfs_open(name="bar.c")
 
-    # tfs_close(2)
-
-    # with open(DEFAULT_DISK_NAME, 'rb') as f:
-    #     f.seek(0)
-    #     print(f.read(BLOCK_SIZE))
-    #     print()
-
-    tfs_unmount()
+    #     # test_write_error = tfs_write(0, "hello world!", 9) #FD error
+    #     # print(test_write_error) # -8 error code
+    #     tfs_open(name="bar.c")
+    #
+    # tfs_unmount()
 
 
 if __name__ == "__main__":
